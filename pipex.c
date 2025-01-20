2f7e204598062a844f57481562b438575728c4d1
Riad El Fassi <riel-fas@c2r5p5.1337.ma>
2025-01-20 16:12:42 +0100

    pipex

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 09:18:27 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/01/19 13:57:01 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/01/20 15:22:39 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	child1(char **cmd_str, int *pipe_fd, char **env)
 	close(fd);
 	cmds_execution(cmd_str[2], env);
 }
+
 void	child2(char **cmd_str, int *pipe_fd, char **env)
 {
 	int	fd;
@@ -44,6 +45,7 @@ void	child2(char **cmd_str, int *pipe_fd, char **env)
 	close(fd);
 	cmds_execution(cmd_str[3], env);
 }
+
 void	pipe_execution(char **av, char **env)
 {
 	int		pipe_fd[2];
@@ -55,15 +57,15 @@ void	pipe_execution(char **av, char **env)
 		perror("Pipe error");
 		exit(EXIT_FAILURE);
 	}
-	child_id1 = fork();
-	if (child_id1 == -1)
+	child_id1 = fork();//forking child1
+	if (child_id1 == -1)//forking error
 	{
 		perror("Fork error");
 		exit(EXIT_FAILURE);
 	}
-	if (child_id1 == 0) // Child 1
+	if (child_id1 == 0)//succes fork
 	{
-		close(pipe_fd[0]);  // Close unused read end
+		close(pipe_fd[0]);
 		child1(av, pipe_fd, env);
 		exit(EXIT_SUCCESS);
 	}
@@ -85,8 +87,6 @@ void	pipe_execution(char **av, char **env)
 	waitpid(child_id2, NULL, 0); // Wait for child 2
 }
 
-
-
 void	cmds_execution(char *cmd_str, char **env)
 {
 	char	**split_cmds_line;
@@ -102,57 +102,15 @@ void	cmds_execution(char *cmd_str, char **env)
 	exec_path = get_the_path(split_cmds_line[0], env);
 	if (!exec_path || execve(exec_path, split_cmds_line, env) == -1)
 	{
-		ft_putstr_fd("hona exec path == ", 2);
-		ft_putstr_fd(exec_path, 2);
-		ft_putstr_fd("\n", 2);
-		ft_putstr_fd("split cmd == ", 2);
-		ft_putstr_fd(split_cmds_line[0], 2);
-		ft_putstr_fd("\n", 2);
-
 		ft_putstr_fd("pipex: command not found: ", 2);
 		ft_putendl_fd(split_cmds_line[0], 2);
 		x = 0;
 		while (split_cmds_line[x])
 			free(split_cmds_line[x++]);
 		free(split_cmds_line);
-		// if (exec_path)
-		// 	free(exec_path);
 		exit(EXIT_FAILURE);
 	}
 }
-
-// void	pipe_execution(char **av, char **env)
-// {
-// 	int		pipe_fd[2];
-// 	pid_t	child_id1;
-// 	pid_t	child_id2;
-
-// 	if (pipe(pipe_fd) == -1)
-// 	{
-// 		perror("Pipe error");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	child_id1 = fork();
-// 	if (child_id1 == -1)
-// 	{
-// 		perror("Fork error");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	if (child_id1 == 0)
-// 		child1(av, pipe_fd, env);
-// 	child_id2 = fork();
-// 	if (child_id2 == -1)
-// 	{
-// 		perror("Fork error");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	if (child_id2 == 0)
-// 		child2(av, pipe_fd, env);
-// 	close(pipe_fd[0]);
-// 	close(pipe_fd[1]);
-// 	waitpid(child_id1, NULL, 0);
-// 	waitpid(child_id2, NULL, 0);
-// }
 
 int	main(int ac, char **av, char **env)
 {
