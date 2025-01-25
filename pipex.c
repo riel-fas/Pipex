0922d26222d1428a38cbce77928661405950bde5
Riad El Fassi <riel-fas@c2r6p9.1337.ma>
2025-01-25 20:27:45 +0100

    pipex

--- pipex.c
+++ pipex.c
@@ -6,46 +6,46 @@
 /*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 09:18:27 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/01/23 16:37:41 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/01/25 19:20:25 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void child1(char **cmd_str, int *pipe_fd, char **env)
+void	child1(char **cmd_str, int *pipe_fd, char **env)
 {
-    int fd = open(cmd_str[1], O_RDONLY);
-    if (fd == -1) {
-        perror("Error opening infile");
-        exit(EXIT_FAILURE);
-    }
+	int	fd;
 
-    dup2(fd, STDIN_FILENO);         // Redirect infile to stdin
-    close(fd);                      // Close file descriptor after dup2
-    dup2(pipe_fd[1], STDOUT_FILENO); // Redirect stdout to pipe write end
-    close(pipe_fd[0]);             // Close read end, not used in this child
-    close(pipe_fd[1]);             // Close write end after dup2
-
-    // Execute command
-    cmds_execution(cmd_str[2], env);
+	fd = open(cmd_str[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("No such file or directory");
+		exit(EXIT_FAILURE);
+	}
+	dup2(fd, 0);
+	// close(fd);
+	dup2(pipe_fd[1], 1);
+	close(pipe_fd[0]);
+	// close(pipe_fd[1]);
+	cmds_execution(cmd_str[2], env);
 }
 
-void child2(char **cmd_str, int *pipe_fd, char **env)
+void	child2(char **cmd_str, int *pipe_fd, char **env)
 {
-    int fd = open(cmd_str[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1) {
-        perror("Error opening outfile");
-        exit(EXIT_FAILURE);
-    }
+	int	fd;
 
-    dup2(pipe_fd[0], STDIN_FILENO); // Redirect pipe read end to stdin
-    close(pipe_fd[1]);             // Close write end, not used in this child
-    close(pipe_fd[0]);             // Close read end after dup2
-    dup2(fd, STDOUT_FILENO);        // Redirect stdout to outfile
-    close(fd);                      // Close file descriptor after dup2
-
-    // Execute command
-    cmds_execution(cmd_str[3], env);
+	fd = open(cmd_str[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+	{
+		perror("Error opening outfile");
+		exit(EXIT_FAILURE);
+	}
+	dup2(fd, 1);
+	dup2(pipe_fd[0], 0);
+	// close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	// close(fd);
+	cmds_execution(cmd_str[3], env);
 }
 
 void	pipe_execution(char **av, char **env)
@@ -56,65 +56,57 @@ void	pipe_execution(char **av, char **env)
 
 	if (pipe(pipe_fd) == -1)
 	{
-    	perror("pipe failed");
-    	exit(EXIT_FAILURE);
+		perror("pipe failed");
+		exit(EXIT_FAILURE);
 	}
-	// First child process
 	child_id1 = fork();
 	if (child_id1 == -1)
 		perror("Fork error");
 	if (child_id1 == 0)
 	{
-		close(pipe_fd[0]); // Close unused read end
+		close(pipe_fd[0]);
 		child1(av, pipe_fd, env);
 	}
-
-	// Second child process
 	child_id2 = fork();
 	if (child_id2 == -1)
 		perror("Fork error");
 	if (child_id2 == 0)
 	{
-		close(pipe_fd[1]); // Close unused write end
+		close(pipe_fd[1]);
 		child2(av, pipe_fd, env);
 	}
-
-	// Parent process
-	close(pipe_fd[0]);  // Close both ends of the pipe
+	close(pipe_fd[0]);
 	close(pipe_fd[1]);
-	waitpid(child_id1, NULL, 0); // Wait for child 1
-	waitpid(child_id2, NULL, 0); // Wait for child 2
+	waitpid(child_id1, NULL, 0);
+	waitpid(child_id2, NULL, 0);
 }
 
-void cmds_execution(char *cmd_str, char **env) {
-    char **split_cmds_line;
-    char *exec_path;
+void	cmds_execution(char *cmd_str, char **env)
+{
+	char	**split_cmds_line;
+	char	*exec_path;
 
-    split_cmds_line = com_pars(cmd_str);
-    if (!split_cmds_line || !split_cmds_line[0]) {
-        ft_putstr_fd("ERROR: Invalid command\n", 2);
-        exit(EXIT_FAILURE);
-    }
-
-    exec_path = get_the_path(split_cmds_line[0], env);
-    if (!exec_path) {
-        ft_putstr_fd("pipex: command not found: ", 2);
-        ft_putendl_fd(split_cmds_line[0], 2);
-        ft_free_tab(split_cmds_line);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Executing: %s\n", exec_path);
-    for (int i = 0; split_cmds_line[i]; i++)
-        printf("Arg %d: %s\n", i, split_cmds_line[i]);
-
-    if (execve(exec_path, split_cmds_line, env) == -1) {
-        perror("execve failed");
-        ft_free_tab(split_cmds_line);
-        exit(EXIT_FAILURE);
-    }
-
-    ft_free_tab(split_cmds_line); // Free arguments after execve
+	split_cmds_line = com_pars(cmd_str);
+	if (!split_cmds_line || !split_cmds_line[0])
+	{
+		ft_putstr_fd("ERROR: Invalid command\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	exec_path = get_the_path(split_cmds_line[0], env);
+	if (!exec_path)
+	{
+		ft_putstr_fd("pipex: command not found: ", 2);
+		ft_putendl_fd(split_cmds_line[0], 2);
+		ft_free_tab(split_cmds_line);
+		exit(EXIT_FAILURE);
+	}
+	if (execve(exec_path, split_cmds_line, env) == -1)
+	{
+		perror("execve failed");
+		ft_free_tab(split_cmds_line);
+		exit(EXIT_FAILURE);
+	}
+	ft_free_tab(split_cmds_line);
 }
 
 int	main(int ac, char **av, char **env)
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: riel-fas <riel-fas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 10:01:08 by riel-fas          #+#    #+#             */
-/*   Updated: 2025/01/23 13:59:48 by riel-fas         ###   ########.fr       */
+/*   Updated: 2025/01/25 18:14:08 by riel-fas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include "libft/libft.h"
+# include <string.h>
 
 void	cmds_execution(char *cmd_str, char **env);
 char	*get_the_path(char *cmd, char **env);
 void	ft_free_tab(char **tab);
-char **com_pars(char *cmd_str);
+char	**com_pars(char *cmd_str);
 
 
 
